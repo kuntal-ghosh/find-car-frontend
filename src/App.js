6c7a62a3c35cdde8a3220086a106fe4b1e0a927e
kuntal-ghosh <kuntalghosh.ewu@gmail.com>
2020-12-11 08:01:43 +0600

    routes were added in this file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { Route,Switch } from 'react-router-dom';
+import Layout from './Hoc/Layout'
+import Button from 'react-bootstrap/Button';
+import HomePage from './pages/Homepage/HomePage';
+import SearchResult from './pages/SearchResult/SearchResult';
+import AddCars from './pages/AddCars/AddCars';
+import CarDetails from './pages/CarDetails/CarDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <Layout>
+      <Switch>
+        <Route exact path="/">
+          <HomePage></HomePage>
+        </Route>
+        <Route exact path="/search_result/:name">
+          <SearchResult></SearchResult>
+        </Route>
+        <Route exact path="/add-car">
+          <AddCars />
+        </Route>
+        <Route exact path="/car-details/:id">
+          <CarDetails />
+        </Route>
+
+      </Switch>
+      
+    </Layout>
   );
 }
 
